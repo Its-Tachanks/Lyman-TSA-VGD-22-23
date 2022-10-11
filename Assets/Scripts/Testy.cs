91c76b26e81f4a8d591947bc4270d1530b745226
Its-Tachanks <joshuaew06@gmail.com>
2022-10-10 21:14:12 -0400

    Update Testy.cs

--- Assets/Scripts/Testy.cs
+++ Assets/Scripts/Testy.cs
@@ -7,7 +7,7 @@ public class Testy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,5 +19,16 @@ public class Testy : MonoBehaviour
         }
 
         Debug.Log(Test());
+    }
+
+    public static T[] GetComponentsInHierarchy<T>(this GameObject go)
+    {
+        List<T> components = new List<T>();
+        components.AddRange(go.GetComponents<T>());
+        foreach (Transform child in go.transform)
+        {
+            components.AddRange(child.gameObject.GetComponentsInHierarch<T>());
+        }
+        return components.ToArray();
     }
 }
